a9fa7593ab0b9727d703cdd0bc5d74d1d1e88dc3
K-Basket <koshykkv@gmail.com>
2023-05-16 23:16:03 +0300

    add orientation images Api

--- src/components/Api/api.js
+++ src/components/Api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '7971179-456b552bdb2500af743f56cc5';
-const PARAMS = 'image_type=photo&per_page=12';
+const PARAMS = 'image_type=photo&per_page=12&orientation=horizontal';
 
 export async function getImages(searchData, page) {
   const { data } = await axios.get(
